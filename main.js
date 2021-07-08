44228e5e71b51a87397e8a2314d08d9891d091c7
ERRrOR404 <error99998@gmail.com>
2021-07-08 19:09:54 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(window).load(function() {
+    $('#m_title').opacity = 0;
+    $('#m_icon_div').opacity = 0;
+    $('#m_get_github').opacity = 0;
+    $('#m_get_start').opacity = 0;
+    $('#copyright').opacity = 0;
+    $('#m_get_bell').opacity = 0;
+});
+
+function changeIcon(){
+    var icon = document.getElementById("m_get_bell_icon");
+
+    if(icon.className == "far fa-bell")
+        icon.className = "far fa-bell-slash";
+    else
+        icon.className = "far fa-bell";
+}
