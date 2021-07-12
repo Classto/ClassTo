c6e5b1760db5277d4e65fa0bd91ae522f6ca1593
ERRrOR404 <error99998@gmail.com>
2021-07-12 13:13:41 +0900

    feat: create router.py

--- templates/app.js
+++ templates/app.js
@@ -1,11 +1,11 @@
-$(window).load(function() {
+window.onload = () => {
     $('#m_title').opacity = 0;
     $('#m_icon_div').opacity = 0;
     $('#m_get_github').opacity = 0;
     $('#m_get_start').opacity = 0;
     $('#copyright').opacity = 0;
     $('#m_get_bell').opacity = 0;
-});
+}
 
 function changeIcon(){
     var icon = document.getElementById("m_get_bell_icon");
