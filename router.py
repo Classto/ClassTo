a6bfb8becda588ca1bc0faaaa82f5f028feface2
ERRrOR404 <error99998@gmail.com>
2021-07-13 17:31:52 +0900

    feat: create editor.html

--- router.py
+++ router.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, url_for, redirect, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def main():
+    return render_template('index.html')
+
+@app.route('/editor')
+def editor():
+    return render_template('editor.html')
 
 if __name__ == '__main__':
     app.run()
