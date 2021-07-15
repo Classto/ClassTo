38dd6feeb7836ae630228786a83a15a9053428b2
ERRrOR404 <error99998@gmail.com>
2021-07-15 20:04:27 +0900

    chore: change size of div

--- static/app.js
+++ static/app.js
@@ -5,32 +5,33 @@ window.onload = () => {
   const spl = document.location.href.split('/');
   if (spl[spl.length - 1] === 'editor.html') {
     const today = new Date();
+    let i = 0;
     document.getElementById('e_date').innerText = months[today.getMonth()] + ' ' + today.getDate() + '. ' + today.getFullYear();
       
-    for (let i = 0; i < 14; i++) {
+    for (i = 0; i < 14; i++) {
       var sample = document.createElement('button');
       sample.style.setProperty('background-color', '#FEFEFE');
       sample.style.setProperty('box-shadow', '0px 4px 25px #327DFF40');
       sample.style.setProperty('position', 'absolute');
-      sample.style.setProperty('width', '100%');
-      sample.style.setProperty('height', '22vh');
+      sample.style.setProperty('height', '18vh');
       sample.style.setProperty('top', top_pos + 'vh');
       sample.style.setProperty('border-radius', '1.5vw');
       sample.style.setProperty('border', 'none');
 
       if (i%3 === 0) {
-        sample.style.setProperty('width', '30%');
-        sample.style.setProperty('margin-left', '2%');
+        sample.style.setProperty('width', '25%');
+        sample.style.setProperty('margin-left', '9%');
       } else if (i%3 === 1) {
-        sample.style.setProperty('width', '30%');
-        sample.style.setProperty('margin-left', '35%');
+        sample.style.setProperty('width', '25%');
+        sample.style.setProperty('margin-left', '37.5%');
       } else {
-        sample.style.setProperty('width', '30%');
-        sample.style.setProperty('margin-left', '68%');
-        top_pos += 25;
+        sample.style.setProperty('width', '25%');
+        sample.style.setProperty('margin-left', '66%');
+        top_pos += 22;
       }
 
       document.getElementById('e_schs').appendChild(sample);
+      document.getElementById('e_schs').style.setProperty('height', i*11 - 3 + 'vh');
     }
   } else {
     document.getElementById('m_title').opacity = 0;
