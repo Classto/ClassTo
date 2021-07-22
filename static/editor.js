74df923ea10b25e75de12e78f47d3914ff9dd09e
ERRrOR404 <error99998@gmail.com>
2021-07-22 13:00:32 +0900

    chore: Change font to Noto Sans KR

--- static/editor.js
+++ static/editor.js
@@ -1,12 +1,6 @@
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 var top_pos = 0;
 
-window.onload = () => {
-    const today = new Date();
-    let i = 0;
-    document.getElementById('e_date').innerText = months[today.getMonth()] + ' ' + today.getDate() + '. ' + today.getFullYear();
-}
-
 function changeIcon() {
     let icon = document.getElementById('m_get_bell_icon');
     
