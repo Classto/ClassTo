c613cab5223cc3921841c9bfb5fa426dd0474159
ERRrOR404 <error99998@gmail.com>
2021-08-03 20:50:55 +0900

    chore: delete app.js file

--- static/app.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-  document.getElementById('m_title').opacity = 0;
-  document.getElementById('m_icon_div').opacity = 0;
-  document.getElementById('m_get_github').opacity = 0;
-  document.getElementById('m_get_start').opacity = 0;
-  document.getElementById('copyright').opacity = 0;
-  document.getElementById('m_get_bell').opacity = 0;
-}
